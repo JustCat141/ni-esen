519f0799e1d36cd7c693cb2b6e2a41f5498d2f85
JustCat141 <patreszkaa@gmail.com>
2024-04-09 09:05:56 +0200

    Initial commit

---
+++ src/test/java/com/esen/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.esen.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
