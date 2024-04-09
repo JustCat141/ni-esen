c67270c0a3b2dd714c1740ed0fd4ec221430f6a7
JustCat141 <patreszkaa@gmail.com>
2024-04-09 09:36:22 +0200

    update20240409

---
+++ src/main/java/com/esen/demo/model/Bookstore.java
@@ -0,0 +1,26 @@
+package com.esen.demo.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Map;
+
+@Entity
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class Bookstore {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    @Column(unique = true)
+    private String location;
+    private double priceModifier;
+    private double moneyInCashRegister;
+    @ElementCollection(fetch = FetchType.EAGER)
+    private Map<Book, Integer> inventory;
+}
